d45f9969d9528dbaaa5bbe83f19046603d1ecda5
李杨 <liyang.ls@bytedance.com>
2021-09-14 11:43:50 +0800

    refs

--- gulpfile.js
+++ gulpfile.js
@@ -13,11 +13,15 @@ const babel = require("gulp-babel");
 
 //#region copy
 function copyClean() {
-  return gulp.src(["dest/static/**"], { read: false }).pipe(clean());
+  return gulp.src(["dest/static/**"], {
+    read: false
+  }).pipe(clean());
 }
+
 function copyCompile() {
   return gulp.src(["src/static/**"]).pipe(gulp.dest("dest/static/"));
 }
+
 function copyWatch() {
   gulp.watch(["src/static/**"], gulp.series(copyClean, copyCompile));
   gulp.series(copyClean, copyCompile)();
@@ -27,8 +31,11 @@ function copyWatch() {
 // https://v3.gulpjs.com.cn/docs/recipes/browserify-with-globs/
 //#region js编译
 function jsClean() {
-  return gulp.src("dest/js/*.js", { read: false }).pipe(clean());
+  return gulp.src("dest/js/*.js", {
+    read: false
+  }).pipe(clean());
 }
+
 function jsCompile() {
   return gulp
     .src(["./src/pages/*/index.js"])
@@ -59,56 +66,70 @@ function jsCompile() {
     )
     .pipe(gulp.dest("dest/js"));
 }
+
 function jsWatch() {
   gulp.watch(["./src/pages/*/index.js"], gulp.series(jsClean, jsCompile));
   gulp.series(jsClean, jsCompile)();
 }
 //#endregion
 
+
 //#region css编译
 function cssClean() {
-  return gulp.src("dest/styles/*.css", { read: false }).pipe(clean());
+  return gulp.src("dest/styles/*.css", {
+    read: false
+  }).pipe(clean());
 }
+
 function cssCompile() {
   return (
     gulp
-      .src(["./src/pages/*/index.less"])
-      // 压缩为一行
-      .pipe(minifyCss())
-      .pipe(
-        less({
-          plugins: [autoprefix],
-        })
-      )
-      .pipe(
-        rename((filePath) => {
-          return {
-            dirname: "",
-            basename: filePath.dirname,
-            extname: ".css",
-          };
-        })
-      )
-      .pipe(gulp.dest("dest/styles"))
+    .src(["./src/pages/*/index.less"])
+    .pipe(
+      less({
+        plugins: [autoprefix],
+      })
+    )
+    // 压缩为一行
+    // .pipe(minifyCss())
+    .pipe(
+      rename((filePath) => {
+        return {
+          dirname: "",
+          basename: filePath.dirname,
+          extname: ".css",
+        };
+      })
+    )
+    .pipe(gulp.dest("dest/styles"))
   );
 }
+
 function cssWatch() {
   gulp.watch(["./src/pages/*/index.less"], gulp.series(cssClean, cssCompile));
   gulp.series(cssClean, cssCompile)();
 }
 //#endregion
 
+
 //#region html编译
 function htmlClean() {
-  return gulp.src("dest/*.html", { read: false }).pipe(clean());
+  return gulp.src("dest/*.html", {
+    read: false
+  }).pipe(clean());
 }
+
 function htmlCompile() {
   const list = fs.readdirSync("src/pages/");
   const data = list.reduce((prev, pageName) => {
+    let json = {}
+    try {
+      json = fs.readJSONSync(`src/pages/${pageName}/index.json`)
+    } catch (e) {}
     return {
       ...prev,
       [pageName]: {
-        ...fs.readJSONSync(`src/pages/${pageName}/index.json`),
+        ...json,
         pageName,
       },
     };
@@ -127,9 +148,10 @@ function htmlCompile() {
     )
     .pipe(gulp.dest("dest"));
 }
+
 function htmlWatch() {
   gulp.watch(
-    ["src/pages/*/index.json", "./src/pages/*/index.ejs", "./src/layout/*.ejs"],
+    ["src/pages/*/index.json", "./src/pages/*/*.ejs"],
     gulp.series(htmlClean, htmlCompile)
   );
   gulp.series(htmlClean, htmlCompile)();
