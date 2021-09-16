c0104c21ef229ba4f390b33f94fc76b2521dbcbc
liyang <20479293@qq.com>
2021-09-17 02:32:28 +0800

    提交按钮点击

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -1,7 +1,7 @@
 $(function () {
-  const phoneDialog = $(".phone-dialog");
-  const phoneElem = $(".phone-dialog .input");
-  const imgbg = $(".phone-dialog .bg").attr("data-src");
+  var phoneDialog = $(".phone-dialog");
+  var phoneElem = $(".phone-dialog .input");
+  var imgbg = $(".phone-dialog .bg").attr("data-src");
   $(".phone-dialog .bg").attr("src", imgbg);
 
   // 关闭手机输入
@@ -11,41 +11,56 @@ $(function () {
 
   // 显示手机输入
   $(
-    ".action-try, .action-try-1, .action-try-2, .action-try-3, .action-try-4"
+    ".action-try, .action-try-1, .action-try-2, .action-try-3, .action-try-4, .try-action-5"
   ).on("click", function () {
     phoneDialog.show();
   });
 
-  // let loading = false;
-  // $(".submit-action-1, .submit-action-2").on("click", function () {
-  //   if (loading) {
-  //     return;
-  //   }
-  //   const phone = phoneElem.val();
-  //   if (!/^1\d{10}$/.test(phone)) {
-  //     return alert("请输入正确格式的手机号码");
-  //   }
-  //   loading = true;
-  //   try {
-  //     $.post("https://open.workec.com/v2/customer/addCustomer", {
-  //       corpid: 12912051,
-  //       APPID: "APPID788470583934320640",
-  //       APPSECRET: "PXn07J6md8X0vIJxKHd",
-  //       list: [
-  //         {
-  //           followUserId: 7689179,
-  //           mobile: phone,
-  //           name: "",
-  //         },
-  //       ],
-  //       optUserId: 16618741,
-  //       onerror,
-  //     }).catch(function () {
-  //       console.log(99992);
-  //     });
-  //   } catch (e) {
-  //     console.error(e);
-  //     console.log(9999333);
-  //   }
-  // });
+  var loading = false;
+  $(".submit-action-1, .submit-action-2").on("click", function () {
+    if (loading) {
+      return;
+    }
+    var phone = phoneElem.val();
+    if (!/^1\d{10}$/.test(phone)) {
+      return alert("请输入正确格式的手机号码");
+    }
+    loading = true;
+    var data = {
+      corpid: 12912051,
+      APPID: "APPID788470583934320640",
+      APPSECRET: "PXn07J6md8X0vIJxKHd",
+      list: [
+        {
+          followUserId: 7689179,
+          mobile: phone,
+          name: "",
+        },
+      ],
+      optUserId: 16618741,
+    };
+    $.ajax({
+      url: "https://open.workec.com/v2/customer/addCustomer",
+      type: "post",
+      dataType: "JSON",
+      data: JSON.stringify(data),
+      timeout: 10000,
+      success(rsp) {
+        if (
+          rsp.code === 200 &&
+          rsp.data.successIdList &&
+          rsp.data.successIdList.length > 0
+        ) {
+          alert("数据保存成功");
+          phoneElem.val("");
+        }
+      },
+      error() {
+        alert("数据提交失败");
+      },
+      complete() {
+        loading = false;
+      },
+    });
+  });
 });
