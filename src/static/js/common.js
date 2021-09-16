7b0e4dec6ddfc668538768d4de9917403102ca0e
liyang <20479293@qq.com>
2021-09-17 01:46:35 +0800

    refs

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -1,5 +1,9 @@
 $(function () {
-  $(".action-try").on("click", function () {
+  const imgbg = $(".phone-dialog .bg").attr("data-src");
+  $(".phone-dialog .bg").attr("src", imgbg);
+  $(
+    ".action-try, .action-try-1, .action-try-2, .action-try-3, .action-try-4"
+  ).on("click", function () {
     alert(111);
   });
 });
